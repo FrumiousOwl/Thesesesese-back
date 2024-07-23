4a12f7239e4a037a1c7731bb8b71793445891439
Jamil T. Calucop <jcalucop@itsrvr01.com>
2024-07-23 13:45:36 +0800

    addCors

--- srrf/Program.cs
+++ srrf/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
+builder.Services.AddCors();
 builder.Services.AddDbContext<SrrfContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SrrfContext")));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<ICategory, CategoryRepository> ();
@@ -25,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000"));
 
 app.UseHttpsRedirection();
 
